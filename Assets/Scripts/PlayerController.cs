216625aa48ed92e764a15f347a5556d1fed0c57a
Joseph Quested <josephquested@gmail.com>
2017-06-09 16:38:18 +1200

    character controller added and character moving

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// SYSTEM //
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// MOVEMENT //
+
+	Rigidbody2D rb;
+
+	public float speed;
+
+	public void ReceiveMovement (float horizontal, float vertical)
+	{
+		Vector2 movement = GetMovementVector(horizontal, vertical);
+		Move(movement);
+	}
+
+	Vector2 GetMovementVector (float horizontal, float vertical)
+	{
+		if (vertical == 1) return Vector2.up;
+		if (horizontal == 1) return Vector2.right;
+		if (vertical == -1) return Vector2.down;
+		if (horizontal == -1) return Vector2.left;
+		return Vector2.zero;
+	}
+
+	void Move (Vector2 movement)
+	{
+		rb.AddForce(movement * speed, ForceMode2D.Impulse);
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	// SYSTEM //
+
+	PlayerController controller;
+
+	void Start ()
+	{
+		controller = GetComponent<PlayerController>();
+	}
+
+	void FixedUpdate ()
+	{
+		UpdateMovement();
+	}
+
+	// INPUTS //
+
+	void UpdateMovement ()
+	{
+		controller.ReceiveMovement(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+	}
+}
